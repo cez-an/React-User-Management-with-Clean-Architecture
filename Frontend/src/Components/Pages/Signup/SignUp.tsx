e3ae10faffe4b62a558a0eb35a79be92cf7feec5
Cez-an <cezan.git@gmail.com>
2025-12-24 23:59:06 +0530

    updated

--- Frontend/src/Components/Pages/Signup/SignUp.tsx
+++ Frontend/src/Components/Pages/Signup/SignUp.tsx
@@ -68,7 +68,7 @@ const Signup = () => {
                 {...register("name", {
                   required: "Full name is required",
                   minLength: {
-                    value: 6,
+                    value: 3,
                     message: "Name must be at least 3 characters",
                   },
                 })}
